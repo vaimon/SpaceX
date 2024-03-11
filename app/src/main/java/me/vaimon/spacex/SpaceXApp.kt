5784c90308c19e7e2c3e44c3ab4c02ab3a4ec64e
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2024-03-11 10:33:55 +0300

    Initial commit

---
+++ app/src/main/java/me/vaimon/spacex/SpaceXApp.kt
@@ -0,0 +1,7 @@
+package me.vaimon.spacex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpaceXApp: Application()
---
+++ app/src/main/java/me/vaimon/spacex/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.vaimon.spacex.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.vaimon.spacex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
